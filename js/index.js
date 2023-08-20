e48f25d1bc8d1ae422a5d488b3cc4dd0d9b2fcc0
mkasiam <kawsarahmmed10001@gmail.com>
2023-08-20 16:36:50 +0600

    coupon code related isuue fixed

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 let total = 0;
-// Adding the selected items in the cart 
+// Adding the selected items in the cart
 function handleCLik(target) {
   const selectedItemContainer = document.getElementById("selected-products");
   const count = selectedItemContainer.childElementCount;
@@ -10,37 +10,24 @@ function handleCLik(target) {
   selectedItemContainer.appendChild(p);
   total = parseInt(total) + parseInt(price);
   document.getElementById("total-price").innerText = total;
-}
-// coupon Code Valid related code 
-const couponCodeInput = document.getElementById("coupon-code");
-const applyButton = document.getElementById("apply-button");
-const cartTotal = total; 
-
-// Event listener for input changes
-couponCodeInput.addEventListener("input", function () {
-  const couponCode = couponCodeInput.value;
-
-  // Enable the apply button if the cart total is >= 200
+  const couponCodeInput = document.getElementById("coupon-code");
+  const applyButton = document.getElementById("apply-button");
+  const cartTotal = total;
   if (cartTotal >= 200) {
-    applyButton.disabled = true;
+      applyButton.disabled = false;
   } else {
-    applyButton.disabled = false;
+      applyButton.disabled = true;
   }
+  // Event listener for apply button click
+  applyButton.addEventListener("click", function () {
+    const couponCode = couponCodeInput.value;
 
-  // Apply discount if coupon code is valid
-  if (couponCode === "SELL200") {
-    const discountedPrice = total * 0.2; // 20% discount
-    document.getElementById(discount).innerText = discountedPrice;
-  }
-});
-
-// // Event listener for apply button click
-// applyButton.addEventListener("click", function () {
-//   const couponCode = couponCodeInput.value;
-
-//   if (couponCode === "SELL200") {
-//     // Apply the discount to the cart or perform related actions
-//     const discountedTotal = cartTotal * 0.8; // 20% discount
-//     console.log("Discount applied. Discounted Total:", discountedTotal);
-//   }
-// });
+    if (couponCode === "SELL200") {
+      // Apply the 20% discount
+      const discountedPrice = cartTotal * 0.2; // 20% discount
+      document.getElementById("discount").innerText = discountedPrice;
+      const grandTotal = total - parseFloat(discountedPrice);
+      document.getElementById("grand-total").innerText = grandTotal;
+    }
+  });
+};
